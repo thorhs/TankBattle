340a85b821a3fe5e69dcfddd20264f882de45c6e
Thorhallur Sverrisson <toti@toti.is>
2017-04-23 11:27:40 +0000

    Added aiming component

--- TankBattle/Source/TankBattle/Private/Tank.cpp
+++ TankBattle/Source/TankBattle/Private/Tank.cpp
@@ -10,6 +10,8 @@ ATank::ATank()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
+
 }
 
 // Called when the game starts or when spawned
@@ -35,6 +37,6 @@ void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 
 void ATank::AimAt(FVector HitLocation)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Tank %s aiming at %s"), *GetName(), *HitLocation.ToString());
+	TankAimingComponent->AimAt(HitLocation);
 }
 
---
+++ TankBattle/Source/TankBattle/Public/TankAimingComponent.h
@@ -0,0 +1,28 @@
+// MIT License
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "TankAimingComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TANKBATTLE_API UTankAimingComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UTankAimingComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	void AimAt(FVector HitLocation);
+	
+};
